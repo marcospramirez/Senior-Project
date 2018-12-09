1a28ebf204fdca73b3f5bac5664d7fb09a32855f
Unknown <mramir14@stedwards.edu>
2018-12-09 14:46:59 -0600

    inserting multiple words

--- spanishDictionary/services/dictionaryService.php
+++ spanishDictionary/services/dictionaryService.php
@@ -6,33 +6,50 @@
     
     $conn = returnConnection();
     if(isset($_POST["newEntry"])){
+        // echo count($_FILES);
+        // echo '<pre>';
+        // var_export($_FILES);
+        // echo '</pre>';
+
+        // foreach ($_FILES["entryAudio"]["name"] as $index => $value){
+        //     echo $index . $value;
+        // }
+
+        $entry = $_POST["entryText"];
+        foreach ($entry as $index => $entryText) {
+            $entryDefinition = $_POST["entryDefinition"][$index];
+            $entryAudio = basename($_FILES["entryAudio"]["name"][$index]);
+
+            $target_dir = "../audio/";
+            $target_file = $target_dir . $entryAudio;
         
-        $entryText = $_POST["entryText"];
-        $entryDefinition = $_POST["entryDefinition"];
-        $entryAudio = basename($_FILES["entryAudio"]["name"]);
-        $target_dir = "../../../uploads";
-        $target_file = $target_dir . $entryAudio;
-        
-        move_uploaded_file($_FILES["entryAudio"]["tmp_name"], $target_file);
-        $sql = "INSERT INTO Entry (entryText, entryDefinition, entryAudioPath) VALUES ('$entryText', '$entryDefinition', '$entryAudio');";
+            move_uploaded_file($_FILES["entryAudio"]["tmp_name"][$index], $target_file);
+            $sql = "INSERT INTO Entry (entryText, entryDefinition, entryAudioPath) VALUES ('$entryText', '$entryDefinition', '$entryAudio');";
 
         
-        if ($conn->query($sql) === TRUE) {
-            $last_id = $conn->insert_id;
-            //echo "New record created successfully. Last inserted ID is: " . $last_id;
-            
-            $addToDictionary = "INSERT INTO entryToDictionary (dictionaryID, entryID) VALUES ('$dictionaryID', '$last_id');";
-            
-            if($conn->query($addToDictionary)){
-                header("Location: ../dictionary.php?dictionaryID=" . $dictionaryID);
-            }
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;  
-        }   
+            if ($conn->query($sql) === TRUE) {
+                $last_id = $conn->insert_id;
+                //echo "New record created successfully. Last inserted ID is: " . $last_id;
+                
+                $addToDictionary = "INSERT INTO entryToDictionary (dictionaryID, entryID) VALUES ('$dictionaryID', '$last_id');";
+                
+                if($conn->query($addToDictionary)){
+                    header("Location: ../dictionary.php?dictionaryID=" . $dictionaryID);
+                }
+            } 
+            else {
+                echo "Error: " . $sql . "<br>" . $conn->error;  
+            }   
         
+        }
+        
+        // $entryText = $_POST["entryText"];
+        // $entryDefinition = $_POST["entryDefinition"];
+        // $entryAudio = basename($_FILES["entryAudio"]["name"]);
+       
     }
     
+    //else, just display the dictionary for the given dictionary id
     else{
     
     
