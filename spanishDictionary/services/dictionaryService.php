0785496350bd05180533e987837a3f30727a1d06
Unknown <mramir14@stedwards.edu>
2019-01-29 15:46:15 -0600

    changed dictionary service

--- spanishDictionary/services/dictionaryService.php
+++ spanishDictionary/services/dictionaryService.php
@@ -116,8 +116,6 @@
 
     function addDictionary($conn){
 
-
-
         if(isset($_POST["dictionaryName"])) {
 
             $classID = $_POST['classroomID'];
@@ -134,54 +132,36 @@
                     //if the new dictionary came with some entries,
                     //then process new entries
                     if(isset($_POST["entryText"])) {
-                        $_POST["newEntry"] = 'true!';
+                        $entry = $_POST["entryText"];
+                        foreach ($entry as $index => $entryText) {
+                            $entryDefinition = $_POST["entryDefinition"][$index];
+                            $entryAudio = basename($_FILES["entryAudio"]["name"][$index]);
+
+                            $target_dir = "../audio/";
+                            $target_file = $target_dir . $entryAudio;
+
+                            move_uploaded_file($_FILES["entryAudio"]["tmp_name"][$index], $target_file);
+                            $sql = "INSERT INTO Entry (entryText, entryDefinition, entryAudioPath) VALUES ('$entryText', '$entryDefinition', '$entryAudio');";
+
+
+                            if ($conn->query($sql) === TRUE) {
+                                $last_id = $conn->insert_id;
+                                //echo "New record created successfully. Last inserted ID is: " . $last_id;
+
+                                $addToDictionary = "INSERT INTO entryToDictionary (dictionaryID, entryID) VALUES ('$dictionaryID', '$last_id');";
+
+                                if($conn->query($addToDictionary)){
+                                    echo "success: dictionary added"; 
+                                }
+                            }
+                            else {
+                                echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+
+                        }
                     }
                 }
             }
-        }
-
-        if(isset($_POST["newEntry"])){
-            // echo count($_FILES);
-            // echo '<pre>';
-            // var_export($_FILES);
-            // echo '</pre>';
-
-            // foreach ($_FILES["entryAudio"]["name"] as $index => $value){
-            //     echo $index . $value;
-            // }
-
-            $entry = $_POST["entryText"];
-            foreach ($entry as $index => $entryText) {
-                $entryDefinition = $_POST["entryDefinition"][$index];
-                $entryAudio = basename($_FILES["entryAudio"]["name"][$index]);
-
-                $target_dir = "../audio/";
-                $target_file = $target_dir . $entryAudio;
-
-                move_uploaded_file($_FILES["entryAudio"]["tmp_name"][$index], $target_file);
-                $sql = "INSERT INTO Entry (entryText, entryDefinition, entryAudioPath) VALUES ('$entryText', '$entryDefinition', '$entryAudio');";
-
-
-                if ($conn->query($sql) === TRUE) {
-                    $last_id = $conn->insert_id;
-                    //echo "New record created successfully. Last inserted ID is: " . $last_id;
-
-                    $addToDictionary = "INSERT INTO entryToDictionary (dictionaryID, entryID) VALUES ('$dictionaryID', '$last_id');";
-
-                    if($conn->query($addToDictionary)){
-                        echo "success: dictionary added"; 
-                    }
-                }
-                else {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-
-            }
-            
-            // $entryText = $_POST["entryText"];
-            // $entryDefinition = $_POST["entryDefinition"];
-            // $entryAudio = basename($_FILES["entryAudio"]["name"]);
-           
         }
 
     }
