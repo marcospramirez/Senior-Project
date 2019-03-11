e1141e556622a10b6f51a91564fdc8543d33e50e
Unknown <mramir14@stedwards.edu>
2019-03-10 21:36:50 -0500

    work from today

--- spanishDictionary/services/db.php
+++ spanishDictionary/services/db.php
@@ -16,6 +16,12 @@ function returnConnection(){
       die("Connection failed: " . $conn->connect_error);
   }
 
+  // if (!$conn->set_charset("utf8")) {
+  //     printf("Error loading character set utf8: %s\n", $conn->error);
+  // } else {
+  //     printf("Current character set: %s\n", $conn->character_set_name());
+  // }
+
   return $conn;
 }
 
